6d9eb15ca2f61b4e65a45870fad1e2aee47049bd
vketh1 <121007613+vketh@users.noreply.github.com>
2022-12-19 07:59:29 +0000

    Initial commit

---
+++ keep_alive.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from threading import Thread
+import random
+
+
+app = Flask('')
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+def run():
+  app.run(
+		host='0.0.0.0',
+		port=8080
+	)
+
+def keep_alive():
+	'''
+	Creates and starts new thread that runs the function run.
+	'''
+	t = Thread(target=run)
+	t.start()
---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from keep_alive import keep_alive
+from discord.ext import commands
+
+bot = commands.Bot(
+	command_prefix="!", 
+	case_insensitive=True 
+)
+
+bot.author_id = 756816729992331304 
+
+@bot.event 
+async def on_ready():
+    print("logged in to " + str(bot.user))
+
+extensions = [
+	'cogs.cog_example'
+]
+
+if __name__ == '__main__': 
+	for extension in extensions:
+		bot.load_extension(extension) 
+
+keep_alive()  
+bot.run(os.environ['DISCORD_BOT_SECRET']) 
